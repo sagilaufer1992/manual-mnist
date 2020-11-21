e2aed4fd4d93c5ce6ffbf949e54112b986fcb959
Sagi <sagi.laufer23@gmail.com>
2020-11-21 15:46:17 +0200

    cross entropy is done maually

--- mnist.py
+++ mnist.py
@@ -7,7 +7,7 @@ input_size = 784  #28*28
 num_classes = 10
 
 num_epochs = 5
-batch_size = 60
+batch_size = 100
 learning_rate = 0.005
 
 transform = transforms.Compose([transforms.ToTensor(),
@@ -24,6 +24,7 @@ test_loader = torch.utils.data.DataLoader(dataset=mnist_test,
                                           shuffle=False)
 
 
+# our version to leaky ReLU
 def leaky_relu(mat):
     zeros = mat - mat
     positives = torch.maximum(mat, zeros)
@@ -31,70 +32,54 @@ def leaky_relu(mat):
     return positives + negatives
 
 
+# our version to softmax
+def softmax(vector):
+    powered = torch.exp(vector)
+    summed = torch.sum(powered)
+    return powered / summed
+
+
+# our version to CE
+def cross_entropy_loss(output, target):
+    mat = torch.diag(torch.ones(10))
+    res = 0
+
+    for (pred, cls) in zip(output, target):
+        softmax_ = softmax(pred)
+        log_softmax = torch.log(softmax_)
+        mat_cls_ = mat[:, cls]
+        res_ = (log_softmax * mat_cls_).sum()
+
+        res = res - res_
+
+    return res / batch_size
+
+
 class TwoLayers(nn.Module):
     def __init__(self, input_size, output_size):
         super(TwoLayers, self).__init__()
         self.linear1 = nn.Linear(input_size, 100)
-        self.linear2 = nn.Linear(350, 100)
-        self.linear3 = nn.Linear(100, output_size)
+        self.linear2 = nn.Linear(100, output_size)
 
     def forward(self, x):
         out1 = self.linear1(x)
-        return self.linear3(leaky_relu(out1))
+        return self.linear2(leaky_relu(out1))
 
 
 model = TwoLayers(input_size, num_classes)
-
-
-def my_loss(output, target):
-    mat = torch.tensor([
-        [0.85, 0, 0, 0, 0, 0, 0, 0, 0.02, 0],  # 0
-        [0, 0.9, 0, 0, 0, 0, 0, 0.1, 0, 0],  # 1
-        [0, 0, 0.95, 0, 0, 0, 0, 0, 0, 0],  # 2
-        [0., 0, 0, 0.9, 0, 0, 0, 0, 0.02, 0],  # 3
-        [0., 0, 0, 0, 0.9, 0, 0, 0, 0.0, 0],  # 4
-        [0., 0, 0, 0, 0, 0.95, 0, 0, 0.02, 0],  # 5
-        [0.05, 0, 0.05, 0.02, 0, 0, 0.9, 0, 0.02, 0],  # 6
-        [0., 0.1, 0, 0, 0, 0, 0, 0.9, 0., 0],  # 7
-        [0.05, 0, 0, 0.1, 0, 0, 0.1, 0, 0.9, 0.1],  # 8
-        [0.05, 0, 0, 0, 0.08, 0.05, 0, 0, 0.02, 0.9],  # 9
-    ])
-
-    mat = torch.diag(torch.ones(10))
-
-    res = 0
-    for (pred, cls) in zip(output, target):
-        softmax_ = nn.Softmax(dim=0)(pred)
-        log_softmax = torch.log(softmax_)
-        mat_cls_ = mat[:, cls]
-        res_ = (log_softmax * mat_cls_).sum()
-        # res_ = log_softmax[cls]
-        # print("softmax: ", softmax_, softmax_.sum(), "\nlog_softmax: ", log_softmax, "\nmat_cls: ", mat_cls_, "\nres: ", res_)
-
-        res = res - res_
-    # print(res)
-    return res / batch_size
-
 
 # Loss and Optimizer
 # Softmax is internally computed.
 ce_loss = nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.85)
 
-cnt = 0
-
 for epoch in range(num_epochs):
-    # if cnt > 0:
-    #     break
     for i, (images, labels) in enumerate(train_loader):
-        # if cnt > 1:
-        #     break
         images = images.view(-1, 28 * 28)
         # Forward + Backward + Optimize
         optimizer.zero_grad()
         outputs = model(images)
-        loss = ce_loss(outputs, labels)
-        # exit(-1)
+        loss = cross_entropy_loss(outputs, labels)
         loss.backward()
         optimizer.step()
 
